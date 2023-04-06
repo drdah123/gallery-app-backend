dbe913a4ee6fb2d885d4aa7efa21c7ab85ffa8fc
drdah123 <108929184+drdah123@users.noreply.github.com>
2023-04-07 01:19:19 +0300

    add timestamp

--- models/user.js
+++ models/user.js
@@ -1,27 +1,30 @@
 import mongoose from 'mongoose';
 const { Schema } = mongoose;
-const userSchema = new Schema({
-  name: {
-    type: String,
-    required: true,
-  },
-  email: {
-    type: String,
-    required: true,
-  },
-  password: {
-    type: String,
-    required: true,
-  },
-  likes: [
-    {
-      post: {
-        type: Schema.Types.ObjectId,
-        ref: 'Post',
-        required: true,
-      },
+const userSchema = new Schema(
+  {
+    name: {
+      type: String,
+      required: true,
     },
-  ],
-});
+    email: {
+      type: String,
+      required: true,
+    },
+    password: {
+      type: String,
+      required: true,
+    },
+    likes: [
+      {
+        post: {
+          type: Schema.Types.ObjectId,
+          ref: 'Post',
+          required: true,
+        },
+      },
+    ],
+  },
+  { timestamps: true }
+);
 const Post = mongoose.model('User', userSchema);
 export default Post;
